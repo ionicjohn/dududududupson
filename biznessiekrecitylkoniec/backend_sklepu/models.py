cd573d08efbcf42c7f356582eb732294b1afc4f4
ionicjohn <39832508+ionicjohn@users.noreply.github.com>
2024-04-26 14:30:17 +0200

    produkty-baza-danych
    dodano field z produktami

--- biznessiekrecitylkoniec/backend_sklepu/models.py
+++ biznessiekrecitylkoniec/backend_sklepu/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Users(models.Model):
     username = models.CharField(max_length=30, primary_key=True)
     password = models.CharField(max_length=200)
@@ -15,4 +14,14 @@ class Address(models.Model):
     street = models.CharField(max_length=100)
     house_number = models.CharField(max_length=10)
     apartment_number = models.CharField(max_length=10, null=True, blank=True)
+    
+class Products(models.Model):
+    product_id = models.IntegerField(primary_key=True)
+    product_name = models.CharField(max_length=200)
+    product_desc = models.CharField(max_length=1000)
+    product_category = models.JSONField()
+    product_photos = models.ImageField()
+    product_price_netto = models.FloatField()
+    product_price_brutto = models.FloatField()
+    
     
