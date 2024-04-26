c4089973bdd4e119a377e3c236d217bcf052e86a
Filip Paska <filippaska2005@gmail.com>
2024-04-26 15:30:50 +0200

    Home-Wstępne wyswietlanie produktow w kafelku po 3
    Wstępne wyswietlanie produktow w kafelku po 3

--- biznessiekreci/src/Frontpage/Frontpage.jsx
+++ biznessiekreci/src/Frontpage/Frontpage.jsx
@@ -1,19 +1,39 @@
 import { useState } from 'react'
 import './Frontpage.css'
+//Produkty bedą zczytywane z bazy danych gdy beda w niej dane
+const products = [{
+  "name": "Tytul1",
+  "description": "Opis1"
+},
+{
+  "name": "Tytul2",
+  "description": "Opis2"
+},
+{
+  "name": "Tytul3",
+  "description": "Opis3"
+}
+]
+
+function Product({ id, name, description, price }) {
+  return (
+    <div className='element'>
+      <h3>{name}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
 
 function Frontpage() {
   return (
     <>
       <div className='container'>
-        <div className='element'>
-          1
-        </div>
-        <div className='element'>
-          2
-        </div>
-        <div className='element'>
-          3
-        </div>
+      {products.map(product => (
+        <Product
+          name={product.name}
+          description={product.description}
+        />
+      ))}
       </div>
     </>
   )
