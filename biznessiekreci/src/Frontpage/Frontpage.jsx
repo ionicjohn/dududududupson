16588ae18eb098b003842804409bf3d9c8d9ce9b
Filip Paska <filippaska2005@gmail.com>
2024-04-26 14:22:53 +0200

    Home-Poprawiona literowka w nazwie folderu
    Poprawiona literowka w nazwie folderu

---
+++ biznessiekreci/src/Frontpage/Frontpage.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './Frontpage.css'
+
+function Frontpage() {
+  return (
+    <>
+      <div className='container'>
+        <div className='element'>
+          1
+        </div>
+        <div className='element'>
+          2
+        </div>
+        <div className='element'>
+          3
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Frontpage
