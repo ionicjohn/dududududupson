10b2ce758f3b2571f926742ccb4efecea7dddf1f
rta228 <vectro@onet.pl>
2024-04-26 15:33:38 +0200

    Footer - Created Footer
    (Still Working On)

--- biznessiekreci/src/main.jsx
+++ biznessiekreci/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Frontpage/Frontpage.jsx'
+import Footer from './Footer/Footer.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
 )
