c6fad5a9b1d557673ba9806a5567f5f050d7bf26
WislaJakub2pt <jakub.wisla@vp.pl>
2024-05-17 15:01:28 +0200

    Dodałem slider oraz galerie
    Dodałem slider oraz galerie

--- biznessiekreci/src/main.jsx
+++ biznessiekreci/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Frontpage from './Frontpage/Frontpage.jsx'
+import ImageSlider from './Frontpage/Slider.jsx'
 import './index.css'
+
+const slides = [
+  'Slide 1: Welcome to the slider!',
+  'Slide 2: This is the second slide.',
+  'Slide 3: Here is the third slide.',
+  'Slide 4: Finally, the fourth slide.'
+];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ImageSlider slides={slides} />
     <Frontpage />
   </React.StrictMode>,
 )
